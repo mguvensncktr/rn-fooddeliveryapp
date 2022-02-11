07c6c4e6e543af2854f0cb535f2f111c7d85765f
mguvensncktr <mguvensancaktar@gmail.com>
2022-02-11 14:56:21 +0300

    added assets and constants

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto_Black': require('./assets/fonts/Roboto-Black.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
